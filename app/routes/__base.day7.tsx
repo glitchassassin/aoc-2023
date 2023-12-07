362e5eecd059be3d16bc68ef9c9907c11ec33dcd
Jon <jon.winsley@gmail.com>
2023-12-07 14:55:37 +0000

    importing markdown directly

--- app/routes/__base.day7.tsx
+++ app/routes/__base.day7.tsx
@@ -1,8 +1,8 @@
-import { useLoaderData, type MetaFunction } from "@remix-run/react";
-import fs from "node:fs/promises";
-import Markdown from "react-markdown";
+import type { MetaFunction } from "@remix-run/react";
 import { PuzzleLayout, PuzzleSection } from "~/components/PuzzleLayout";
 import { part1Example, puzzleInput } from "~/solutions/day7/inputs";
+import ProblemStatement1 from "~/solutions/day7/problemStatement1.md";
+import ProblemStatement2 from "~/solutions/day7/problemStatement2.md";
 import { ScoreHands, ScoreHandsWithJokers } from "~/solutions/day7/solution";
 
 const DAY = 7;
@@ -11,32 +11,18 @@ export const meta: MetaFunction = () => {
   return [{ title: `AoC 2023 | Day ${DAY}` }];
 };
 
-export const loader = async () => {
-  const [problemStatement1, problemStatement2] = await Promise.all(
-    ["problemStatement1.md", "problemStatement2.md"].map((file) =>
-      fs.readFile(`./app/solutions/day${DAY}/${file}`).then((f) => f.toString())
-    )
-  );
-  return {
-    problemStatement1,
-    problemStatement2,
-  };
-};
-
 export default function DisplayProblem() {
-  const { problemStatement1, problemStatement2 } =
-    useLoaderData<typeof loader>();
   return (
     <PuzzleLayout day={DAY} title="Camel Cards">
       <h3 className="pt-3 pb-2">Part One</h3>
       <PuzzleSection
-        problemStatement={<Markdown>{problemStatement1}</Markdown>}
+        problemStatement={<ProblemStatement1 />}
         testImplementation={<ScoreHands hands={part1Example} expected={6440} />}
         solution={<ScoreHands hands={puzzleInput} />}
       />
       <h3 className="pt-3 pb-2">Part Two</h3>
       <PuzzleSection
-        problemStatement={<Markdown>{problemStatement2}</Markdown>}
+        problemStatement={<ProblemStatement2 />}
         testImplementation={
           <ScoreHandsWithJokers hands={part1Example} expected={5905} />
         }
--- remix.config.js
+++ remix.config.js
@@ -5,23 +5,4 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
-  serverDependenciesToBundle: [
-    /^rehype.*/,
-    /^remark.*/,
-    /^unified.*/,
-    /^estree.*/,
-    /^unist.*/,
-    /^hast.*/,
-    /^bail.*/,
-    /^trough.*/,
-    /^mdast.*/,
-    /^micromark.*/,
-    /^decode.*/,
-    /^character.*/,
-    /^property.*/,
-    /^space.*/,
-    /^comma.*/,
-    /^react-markdown$/,
-    /^vfile.*/,
-  ]
 };
